cac71ec7e895dcf880d08e8d07ce66a8cc2dfcb3
gemAbhishek <ak.abhishekshah@gmail.com>
2021-05-10 00:44:15 +0530

    initial with code

---
+++ Image Proccessing BAsic/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Image_Proccessing_BAsic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnCyan = new System.Windows.Forms.Button();
+            this.btnGrayAndGray = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.BackColor = System.Drawing.Color.White;
+            this.picOriginal.Location = new System.Drawing.Point(21, 100);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(360, 267);
+            this.picOriginal.TabIndex = 0;
+            this.picOriginal.TabStop = false;
+            // 
+            // picResult
+            // 
+            this.picResult.BackColor = System.Drawing.Color.White;
+            this.picResult.Location = new System.Drawing.Point(620, 100);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(351, 267);
+            this.picResult.TabIndex = 1;
+            this.picResult.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(150, 406);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(92, 42);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnGray.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.818182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGray.Location = new System.Drawing.Point(424, 151);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(137, 43);
+            this.btnGray.TabIndex = 3;
+            this.btnGray.Text = "Gray";
+            this.btnGray.UseVisualStyleBackColor = false;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // btnCyan
+            // 
+            this.btnCyan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnCyan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.818182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCyan.Location = new System.Drawing.Point(424, 231);
+            this.btnCyan.Name = "btnCyan";
+            this.btnCyan.Size = new System.Drawing.Size(137, 43);
+            this.btnCyan.TabIndex = 4;
+            this.btnCyan.Text = "Cyan";
+            this.btnCyan.UseVisualStyleBackColor = false;
+            this.btnCyan.Click += new System.EventHandler(this.btnCyan_Click);
+            // 
+            // btnGrayAndGray
+            // 
+            this.btnGrayAndGray.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnGrayAndGray.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.818182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrayAndGray.Location = new System.Drawing.Point(425, 305);
+            this.btnGrayAndGray.Name = "btnGrayAndGray";
+            this.btnGrayAndGray.Size = new System.Drawing.Size(137, 43);
+            this.btnGrayAndGray.TabIndex = 5;
+            this.btnGrayAndGray.Text = "Cyan&Gray";
+            this.btnGrayAndGray.UseVisualStyleBackColor = false;
+            this.btnGrayAndGray.Click += new System.EventHandler(this.btnGrayAndGray_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1037, 553);
+            this.Controls.Add(this.btnGrayAndGray);
+            this.Controls.Add(this.btnCyan);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.picOriginal);
+            this.Name = "Form1";
+            this.Text = "Image Processing Basic";
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnCyan;
+        private System.Windows.Forms.Button btnGrayAndGray;
+    }
+}
+
---
+++ Image Proccessing BAsic/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Image_Proccessing_BAsic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Opfile = new OpenFileDialog();
+            Opfile.Filter = "Image File (*.bmp,*.jpg)| *.bmp;*.jpg";
+            if(DialogResult.OK==Opfile.ShowDialog())
+            {
+                this.picOriginal.Image = new Bitmap(Opfile.FileName);
+            }
+        }
+
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            Bitmap coppy = new Bitmap((Bitmap)this.picOriginal.Image);
+
+            Method.ConvertToGray(coppy);
+            this.picResult.Image = coppy;
+        }
+
+        private void btnCyan_Click(object sender, EventArgs e)
+        {
+            Bitmap coppy = new Bitmap((Bitmap)this.picOriginal.Image);
+
+            Method.ConvertToCyan(coppy);
+            this.picResult.Image = coppy;
+        }
+
+        private void btnGrayAndGray_Click(object sender, EventArgs e)
+        {
+            Bitmap coppy = new Bitmap((Bitmap)this.picOriginal.Image);
+
+            Method.ConvertToGrayAndGray(coppy);
+            this.picResult.Image = coppy;
+        }
+    }
+}
